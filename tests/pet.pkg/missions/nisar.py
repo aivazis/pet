8deb464434fe362da4b6743f9f6851874adc0472
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-04-09 23:22:00 +0300

    pet.missions: added a simple test for the {nisar} mission simulator

---
+++ tests/pet.pkg/missions/nisar.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2025 all rights reserved
+
+
+"""
+Sanity check: verify that the {pet} package is accessible
+"""
+
+
+# support
+import pet
+import journal
+
+
+# the driver
+def test():
+    # instantiate a mission
+    mission = pet.missions.nisar(name="NISAR")
+    # and a channel
+    channel = journal.info("pet.missions")
+    # ask the mission to report
+    mission.report(channel=channel)
+    # and flush
+    channel.log()
+
+    # all done
+    return
+
+
+# the main entry point
+if __name__ == "__main__":
+    # do...
+    status = test()
+    # and share
+    raise SystemExit(status)
+
+
+# end of file
