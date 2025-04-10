d0ec243be901a98c54b35e41bdb899492e9449b7
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-04-10 21:23:35 +0300

    pet.protocols: the base instrument {mode} now has a default value

--- pkg/pet/protocols/instruments/Mode.py
+++ pkg/pet/protocols/instruments/Mode.py
@@ -15,5 +15,14 @@ class Mode(pet.protocol, family="pet.instruments.modes"):
     Requirements for instrument modes
     """
 
+    # framework hooks
+    @classmethod
+    def pyre_default(cls, **kwds):
+        """
+        Supply a default mode when one is not specified by the user
+        """
+        # instruments are off by default
+        return pet.instruments.modes.off
+
 
 # end of file
