92ba9c5815818e1ffdb041fcf5dbf668a433bd0c
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-04-14 14:15:05 +0300

    pet::utils: made a home for the support utilities that likely to be needed by the low level code

---
+++ lib/pet/utils.h
@@ -0,0 +1,15 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2025 all rights reserved
+
+
+// code guard
+#pragma once
+
+
+// support for utils
+#include "utils/public.h"
+
+
+// end of file
---
+++ lib/pet/utils/forward.h
@@ -0,0 +1,20 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2025 all rights reserved
+
+
+// code guard
+#pragma once
+
+
+// externals
+#include "externals.h"
+
+
+// prime the namespace
+namespace pet::utils {
+}
+
+
+// end of file
