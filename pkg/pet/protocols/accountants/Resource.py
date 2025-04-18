44f8260d739941dce1eee2f73209b6697ca2f3fb
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-04-18 22:26:09 +0300

    pet.protocols: primed the {accountants}

---
+++ pkg/pet/protocols/accountants/Resource.py
@@ -0,0 +1,19 @@
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2025 all rights reserved
+
+
+# support
+import pet
+
+
+# requirements for the quantities accountants care about
+class Resource(pet.protocol, family="pet.resources"):
+    """
+    Requirements for the various quantities that accountants keep track of
+    """
+
+
+# end of file
