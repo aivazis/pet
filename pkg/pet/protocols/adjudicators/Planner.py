71557bd23c7a80e006dfecb4e9eb73e61d25a072
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-04-18 22:26:35 +0300

    pet.protocols: primed the {adjudicators}

---
+++ pkg/pet/protocols/adjudicators/Planner.py
@@ -0,0 +1,36 @@
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2025 all rights reserved
+
+
+# support
+import pet
+
+# parts
+from .. import missions
+
+
+# the requirements for mission planners
+class Planner(pet.protocol, family="pet.adjudicators.planners"):
+    """
+    Requirements for mission planners
+    """
+
+    # the mission
+    mission = missions.mission()
+    mission.doc = "the mission characteristics"
+
+    # the simulation window
+    start = pet.properties.timestamp()
+    start.doc = "the beginning of the simulation window"
+
+    stop = pet.properties.timestamp()
+    stop.doc = "the end of the simulation window"
+
+    dt = pet.properties.dimensional()
+    dt.doc = "the sampling "
+
+
+# end of file
