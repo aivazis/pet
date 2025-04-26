3319795ce6e3d1ebbb818eae40aae05cf206965f
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-04-26 08:22:39 +0300

    pet.protocols: added some interrace requirements to {objective} in {adjudicators}

--- pkg/pet/protocols/adjudicators/Objective.py
+++ pkg/pet/protocols/adjudicators/Objective.py
@@ -15,5 +15,32 @@ class Objective(pet.protocol, family="pet.adjudicators.objectives"):
     Requirements for mission objectives
     """
 
+    # interface obligations
+    @pet.provides
+    def begin(self, mission, start, stop, **kwds):
+        """
+        Begin a mission simulation
+        """
+
+    @pet.provides
+    def forecast(self, mission, t, stop, dt, **kwds):
+        """
+        Examine the proposed time window [t, t+dt] for activity relevant to this objective and
+        suggest a more appropriate timestep
+        """
+
+    @pet.provides
+    def advance(self, mission, t, dt, **kwds):
+        """
+        Examine the proposed time window [t, t+dt] for any activity relevant to this objective and
+        return a list of the necessary actions
+        """
+
+    @pet.provides
+    def end(self, mission, t, stop, **kwds):
+        """
+        Finish a mission simulation
+        """
+
 
 # end of file
