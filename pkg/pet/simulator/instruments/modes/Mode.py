9303ed0ba2cfa85a63998479f700d091f87c7527
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-04-28 23:29:01 +0300

    pet.simulator: added default implementations of the {mode} interface

--- pkg/pet/simulator/instruments/modes/Mode.py
+++ pkg/pet/simulator/instruments/modes/Mode.py
@@ -9,15 +9,28 @@
 import pet
 
 
-# requirements for the SAR beam mode
-class Mode(
-    pet.component,
-    family="pet.instruments.modes.base",
-    implements=pet.protocols.instruments.mode,
-):
+# the base class for instrument modes
+class Mode(pet.component, implements=pet.protocols.instruments.mode):
     """
     The base operational mode for all instrument types
     """
+
+    # interface requirements
+    @pet.export
+    def consumes(self, resource):
+        """
+        Model the {resource} consumption by this mode as a function of time
+        """
+        # no, by default
+        return lambda t: 0
+
+    @pet.export
+    def measures(self, observable):
+        """
+        Check whether this instrument mode can measure the given {observable}
+        """
+        # no, by default
+        return False
 
 
 # end of file
