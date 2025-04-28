5594f07016075618aab543582852203382902424
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-04-28 23:29:22 +0300

    pet.simulator: added an {idle} mode

---
+++ pkg/pet/simulator/instruments/modes/Idle.py
@@ -0,0 +1,31 @@
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2025 all rights reserved
+
+
+# support
+import pet
+
+
+# superclass
+from .Mode import Mode
+
+
+# the state of the instrument when it is on standby
+class Idle(Mode, family="pet.instruments.modes.idle"):
+    """
+    The instrument mode that corresponds to being powered on but idle
+    """
+
+    # metamethods
+    def __str__(self):
+        """
+        Human readable rendering of my state
+        """
+        # easy enough
+        return "idle"
+
+
+# end of file
